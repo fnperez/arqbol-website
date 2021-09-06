66ec7ec3ebad38d0a7c192677a4eb641a5ba6545
Facundo Perez <facundo@outcomelabs.com>
2021-09-06 13:43:27 -0300

    lala

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 80
+const port = process.env.PORT;
 
 app.get('/.well-known/acme-challenge/0kB6MA0nHvXFUOrGN-daVcSgxB7KVrDro-F1cLhSgIw', (req, res) => {
   res.send('0kB6MA0nHvXFUOrGN-daVcSgxB7KVrDro-F1cLhSgIw.vGpzTNVQy4hG0bDBJ9lSLM3ob_e6ckkJ0AG92rnC91M')
